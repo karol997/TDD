bf8fce153a45465d8598aec831301a023bfbe539
karol997 <karol789456@gmail.com>
2021-05-23 19:37:56 +0200

    Add project files.

---
+++ TDDLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TDD
+{
+    public class TDDLibrary
+    {
+
+
+        public static int max(int a, int b, int c)
+        {
+            int maximum = 0;
+            if (a > b) maximum = a;
+            if (a > c) maximum = a;
+            if (a < b) maximum = b;
+            if (a < c) maximum = c;
+            if (b > c) maximum = b;
+            if (c > b) maximum = c;
+            return maximum;
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
